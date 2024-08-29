b64421c982b353a6a095383d194b04f0749ce839
Akshay <akshaykommu1997@gmail.com>
2024-08-29 19:35:34 +0530

    cors changes to allow all

--- menu/src/main/java/com/app/menu/config/WebConfig.java
+++ menu/src/main/java/com/app/menu/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Allow requests from the React app
+                .allowedOrigins("*") // Allow requests from the React app
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
